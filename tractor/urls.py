88e464036392893a03dd961c7c8b029c73a70f9d
wwyyww <yso00302@gmail.com>
2023-08-15 17:47:17 +0900

    [chore] 설정 파일 수정

--- tractor/urls.py
+++ tractor/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path as url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('evidence/', include('evidence.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
+
 ]
