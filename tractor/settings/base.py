9f9d79ce59931181f7cc0e08bbc0e981e22bb1c3
wwyyww <yso00302@gmail.com>
2023-08-15 21:48:50 +0900

    [fix] 파일 구조 변경

--- tractor/settings/base.py
+++ tractor/settings/base.py
@@ -60,7 +60,13 @@ ROOT_URLCONF = 'tractor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR,'evidence/templates'),
+                 
+                 
+                 
+                 
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
