0c3a16fc4e1c58ce59f2f3ac9f6ab53f5a0a38a8
wwyyww <yso00302@gmail.com>
2023-08-15 18:13:36 +0900

    [chore] 설정 파일 수정

--- tractor/urls.py
+++ tractor/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 from django.urls import re_path as url
 from django.views.static import serve
