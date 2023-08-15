d0b7d34071763b19c2cf32ee9217b6ddc95883eb
wwyyww <yso00302@gmail.com>
2023-08-15 21:33:01 +0900

    [fix] 파일 구조 변경

--- tractor/urls.py
+++ tractor/urls.py
@@ -11,7 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('evidence/', include('evidence.urls')),
     path('hello/', views.index),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
-
 ]
