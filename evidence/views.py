104d48580b537c28c36050585940f583ce567b35
wwyyww <yso00302@gmail.com>
2023-08-16 13:45:39 +0900

    [feat] 증거 카테고리, 로그인 정보 연결

--- evidence/views.py
+++ evidence/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.http import HttpResponseNotAllowed
 from .models import Evidence
 from .forms import EvidenceForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
+@login_required
 def write(request):
     if request.method=='GET':
         form=EvidenceForm()
@@ -13,6 +16,7 @@ def write(request):
         form=EvidenceForm(request.POST)
         if form.is_valid():
             evidence=form.save(commit=False)
+            evidence.user=request.user
             if request.FILES.get('file') is not None:
                 evidence.attached=request.FILES["upload"]
             evidence.created_at=timezone.now()
@@ -27,6 +31,16 @@ def lists(request):
 def index(request):
     return render(request, 'evidence/index.html')
 
+@login_required
 def detail(request, pk):
     evidence=Evidence.objects.get(pk=pk)
     return render(request, 'evidence/evidence_detail.html', {'evidence':evidence})
+
+@require_POST
+def delete(request, pk):
+    evidence=get_object_or_404(Evidence, pk=pk)
+    if request.user.is_authenticated:
+        if request.user==evidence.user:
+            evidence.delete()
+            return redirect('evidence:lists')
+    return redirect('evidence:detail', evidence.pk)
