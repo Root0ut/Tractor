0ba22254422bf061e1158293ee0263b8737a8c84
leesooin0422 <78577772+leesooin0422@users.noreply.github.com>
2023-08-16 22:55:30 +0900

    update
    craw_list 전달 받은 화면 수정

--- craw/views.py
+++ craw/views.py
@@ -23,25 +23,49 @@ from craw.models import CrawData
 def index(request):
     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
 
-
 def craw_list(request):
-    craw_list = CrawData.objects.all()
-    page = request.GET.get('page', 1)
+    input_url = request.GET.get('input_url')
+    keyword = request.GET.get('keyword')
+    craw_data_dict = craw(input_url)
+    craw_list=[]
+    for item in craw_data_dict:
+        if keyword in item['comment']:
+                craw_item=CrawData()
 
-    paginator = Paginator(craw_list, 10)
+                craw_item.link=item['link']
+                craw_item.user_id=item['user_id']
+                craw_item.date=item['date']
+                craw_item.comment = item['comment']
 
-    try:
-        page_obj = paginator.get_page(page)
-    except PageNotAnInteger:
-        page = 1
-        page_obj = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages
-        page_obj = paginator.page(page)
+                print(craw_item.comment)
+                
+                craw_list.append(craw)
+                context = {
+                    # 'craw_item' : craw_item, 
+                    'craw_list' : craw_list
+                }
 
-
-    context = {'craw_list': craw_list, 'page_obj':page_obj, 'paginator':paginator}
     return render(request, 'craw/craw_list.html', context)
+
+
+# def craw_list(request):
+#     craw_list = CrawData.objects.all()
+#     page = request.GET.get('page', 1)
+
+#     paginator = Paginator(craw_list, 10)
+
+#     try:
+#         page_obj = paginator.get_page(page)
+#     except PageNotAnInteger:
+#         page = 1
+#         page_obj = paginator.page(page)
+#     except EmptyPage:
+#         page = paginator.num_pages
+#         page_obj = paginator.page(page)
+
+
+#     context = {'craw_list': craw_list, 'page_obj':page_obj, 'paginator':paginator}
+#     return render(request, 'craw/craw_list.html', context)
 
 
 def tractor_main(request):
@@ -92,26 +116,27 @@ def craw(url):
     return result    
 
 
-def catch(request):
-    input_url = request.GET.get('input_url')
-    keyword = request.GET.get('keyword')
-    craw_data_dict = craw(input_url)
-    craw_list=[]
-    for item in craw_data_dict:
-        if keyword in item['comment']:
-                craw_item=CrawData()
+# def catch(request):
+#     input_url = request.GET.get('input_url')
+#     keyword = request.GET.get('keyword')
+#     craw_data_dict = craw(input_url)
+#     craw_list=[]
+#     for item in craw_data_dict:
+#         if keyword in item['comment']:
+#                 craw_item=CrawData()
 
-                craw_item.link=item['link']
-                craw_item.user_id=item['user_id']
-                craw_item.date=item['date']
-                craw_item.comment = item['comment']
+#                 craw_item.link=item['link']
+#                 craw_item.user_id=item['user_id']
+#                 craw_item.date=item['date']
+#                 craw_item.comment = item['comment']
 
-                print(craw_item.comment)
+#                 print(craw_item.comment)
                 
-                craw_list.append(craw)
-                context = {
-                    'craw_list' : craw_list
-                }
+#                 craw_list.append(craw)
+#                 context = {
+#                     # 'craw_item' : craw_item, 
+#                     'craw_list' : craw_list
+#                 }
 
-    return render(request, 'craw/catch.html', context)
+#     return render(request, 'craw/craw_list.html', context)
 
