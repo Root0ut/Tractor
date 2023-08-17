9ff4ec231985fb420f30f329f62f00f291fa7c3c
suv <btsiu@naver.com>
2023-08-18 02:35:19 +0900

    [feat] 로그인 안 한 사용자 접근 제한

--- myinfo/views.py
+++ myinfo/views.py
@@ -5,16 +5,21 @@ from django.views.decorators.http import require_POST
 from myinfo.models import Survey, Answer
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/user/login')
 def index(request):
     return render(request, 'myinfo/myinfo.html')
 
+@login_required(login_url='/user/login')
 def feedback(request):
     survey_list=Survey.objects.order_by('-survey_idx')
     context = { 'survey_list': survey_list}
     return render(request, 'myinfo/feedback.html', context)
 
+
 @csrf_exempt
+@login_required(login_url='/user/login')
 def save_survey(request):
     dto = Answer(username=request.POST["username"],
                  survey_idx=request.POST["survey_idx"], num=request.POST["num"])
