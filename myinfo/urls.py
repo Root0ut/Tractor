fb03c1ade62445f4dbe2e0232d228b9502ff32ed
wwyyww <yso00302@gmail.com>
2023-08-18 02:19:51 +0900

    [feat] 2차 인증 비밀번호 설정

--- myinfo/urls.py
+++ myinfo/urls.py
@@ -5,4 +5,6 @@ app_name='myinfo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('feedback/', views.feedback, name='feedback'),
+    path('second_auth/', views.second_auth, name='second_auth'),
+
 ]
--- myinfo/views.py
+++ myinfo/views.py
@@ -1,8 +1,12 @@
 
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from myinfo.models import Survey, Answer
+from django.contrib.auth.hashers import make_password
+import bcrypt
+from user.models import UserSecondPw
+from user.forms import UserSecondForm
 
 
 def index(request):
@@ -12,3 +16,22 @@ def feedback(request):
     survey_list=Survey.objects.order_by('-survey_idx')
     context = { 'survey_list': survey_list}
     return render(request, 'myinfo/feedback.html', context)
+
+def second_auth(request):
+    if request.method=='GET':
+        return render(request, 'myinfo/twoauth.html')
+    elif request.method=='POST':
+        password=request.POST.get('password').encode('utf-8')
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+        usersecond=UserSecondPw()
+        usersecond.user=request.user
+        usersecond.pw=hashed_password
+        usersecond.save()
+        return redirect('pdfextract:index')
+
+
+def get_second_pw(request):
+    form = UserSecondForm()
+    return render(request, 'movie_form.html', {
+        'form': form,
+    })
