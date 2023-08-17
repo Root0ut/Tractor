31afe9126a30952348353ed1d0fe218ded2cec5b
suv <btsiu@naver.com>
2023-08-17 23:22:46 +0900

    [feat] 피드백 페이지 생성

--- myinfo/views.py
+++ myinfo/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from myinfo.models import Survey, Answer
-
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
 def index(request):
     return render(request, 'myinfo/myinfo.html')
@@ -12,3 +13,11 @@ def feedback(request):
     survey_list=Survey.objects.order_by('-survey_idx')
     context = { 'survey_list': survey_list}
     return render(request, 'myinfo/feedback.html', context)
+
+@csrf_exempt
+def save_survey(request):
+    dto = Answer(username=request.POST["username"],
+                 survey_idx=request.POST["survey_idx"], num=request.POST["num"])
+    dto.save()
+    
+    return render(request, "myinfo/success.html")
